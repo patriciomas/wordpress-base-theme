204f6627ce66a40a7423f1508896fe025a9d7896
Pato+ <patomas@Patos-MacBook-Pro.local>
2016-01-30 12:33:27 -0300

    Better pagination
    Much better pagination options for single and archive pages.

--- pagination.php
+++ pagination.php
@@ -1,4 +1,13 @@
 <div class="post-navigation">
-  <div class="nav-previous"><?php previous_post_link(); ?></div>
-  <div class="nav-next"><?php next_post_link(); ?></div>
+  <div class="divisor"></div>
+  <?php if ( !is_archive() && !is_search() ) { ?>
+    <div class="nav-previous"><?php previous_post_link(); ?></div>
+    <div class="nav-next"><?php next_post_link(); ?></div>
+  <?php }else{
+    the_posts_pagination( array(
+    	'mid_size'  => 2,
+      'prev_text' => __( '<i class="fa fa-chevron-left"></i>' ),
+      'next_text' => __( '<i class="fa fa-chevron-right"></i>' ),
+      ) );
+  }; ?>
 </div>
