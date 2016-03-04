0276442ef2d49ff43f0b61cdaad6ec1076b8e104
Pato+ <patomas@Patos-MacBook-Pro.local>
2016-03-03 21:37:36 -0300

    Better Comments
    Updated the comments template so that it can be edited with the proper
    comment_form array arguments. Also now supports nested replies.

--- comments.php
+++ comments.php
@@ -1,105 +1,111 @@
-<?php // Do not delete these lines
-	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die ('Please do not load this page directly. Thanks!');
-
-	if (!empty($post->post_password)) { // if there's a password
-		if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {  // and it doesn't match the cookie
-			?>
-
-			<p>This post is password protected. Enter the password to view comments.</p>
-
-			<?php
-			return;
-		}
-	}
-
-	/* This variable is for alternating comment background */
-	$oddcomment = 'class="alt" ';
+<?php
+if ( post_password_required() ) {
+	return;
+}
 ?>
 
-<!-- You can start editing here. -->
+<div id="comments" class="comments">
 
-<?php if ($comments) : ?>
-	<div class="comments">
-	<h3><?php comments_number('¡Sea el primero en comentar!', 'Un comentario', '% comentarios' );?></h3>
+	<?php if ( have_comments() ) : ?>
+		<h3 class="comments-title">
+			<?php
+				$comments_number = get_comments_number();
+				if ( 1 === $comments_number ) {
+					/* translators: %s: post title */
+					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title' ), get_the_title() );
+				} else {
+					printf(
+						/* translators: 1: number of comments, 2: post title */
+						_nx(
+							'%1$s comentario en &ldquo;%2$s&rdquo;',
+							'%1$s comentarios en &ldquo;%2$s&rdquo;',
+							$comments_number,
+							'comments title'
+						),
+						number_format_i18n( $comments_number ),
+						get_the_title()
+					);
+				}
+			?>
+		</h3>
 
-	<ol>
+		<?php the_comments_navigation(); ?>
 
-	<?php foreach ($comments as $comment) : ?>
+		<ol class="commentlist">
+			<?php wp_list_comments( 'reply_text=<i class="fa fa-reply"></i> Responder&type=comment&callback=mytheme_comment' ); ?>
+		</ol><!-- .comment-list -->
 
-		<li <?php echo $oddcomment; ?>id="comment-<?php comment_ID() ?>">
-			<?php echo get_avatar( $comment, 32 ); ?>
-			<cite><?php comment_author_link() ?></cite> Says:
-			<?php if ($comment->comment_approved == '0') : ?>
-			<p>Your comment is awaiting moderation.</p>
-			<?php endif; ?>
-			<p><a href="#comment-<?php comment_ID() ?>" title=""><?php comment_date('F jS, Y') ?> at <?php comment_time() ?></a> <?php edit_comment_link('edit','&nbsp;&nbsp;',''); ?></p>
-			<?php comment_text() ?>
-		</li>
+		<?php the_comments_navigation(); ?>
+
+	<?php endif; // Check for have_comments(). ?>
 
 	<?php
-		/* Changes every other comment to a different class */
-		$oddcomment = ( empty( $oddcomment ) ) ? 'class="alt" ' : '';
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="no-comments"><?php _e( 'Comments are closed.' ); ?></p>
+	<?php endif; ?>
+
+	<?php
+
+		$commenter = wp_get_current_commenter();
+		$req = get_option( 'require_name_email' );
+		$aria_req = ( $req ? " aria-required='true'" : '' );
+
+		$fields =  array(
+
+		  'author' =>
+		    '<p class="comment-form-author"><label for="author">' . __( 'Name' ) . '</label> ' .
+		    ( $req ? '<span class="required">*</span>' : '' ) .
+		    '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+		    '" size="30"' . $aria_req . ' /></p>',
+
+		  'email' =>
+		    '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' .
+		    ( $req ? '<span class="required">*</span>' : '' ) .
+		    '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+		    '" size="30"' . $aria_req . ' /></p>',
+
+		);
+
+		$comments_args = array(
+		  'id_form'           => 'commentform',
+		  'class_form'        => 'comment-form',
+		  'id_submit'         => 'submit',
+		  'class_submit'      => 'submit',
+		  'name_submit'       => 'submit',
+		  'title_reply'       => 'Deje su opinión',
+		  'title_reply_to'    => __( 'Leave a Reply to %s' ),
+		  'cancel_reply_link' => __( 'Cancel Reply' ),
+		  'label_submit'      => 'Publicar',
+		  'format'            => 'xhtml',
+
+		  'comment_field' =>  '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) .
+		    '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true">' .
+		    '</textarea></p>',
+
+		  'must_log_in' => '<p class="must-log-in">' .
+		    sprintf(
+		      __( 'You must be <a href="%s">logged in</a> to post a comment.' ),
+		      wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+		    ) . '</p>',
+
+		  'logged_in_as' => '<p class="logged-in-as">' .
+		    sprintf(
+		    __( 'Identificado como %2$s. <a href="%3$s" title="Cerrar sesión">Cerrar sesión?</a>' ),
+		      admin_url( 'profile.php' ),
+		      $user_identity,
+		      wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+		    ) . '</p>',
+
+		  'comment_notes_before' => '<p class="comment-notes">' .
+		    __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) .
+		    '</p>',
+
+		  'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+		);
+
+		comment_form($comments_args);
 	?>
 
-	<?php endforeach; /* end for each comment */ ?>
-
-	</ol>
-
- <?php else : // this is displayed if there are no comments so far ?>
-
-	<?php if ('open' == $post->comment_status) : ?>
-
-		<div class="comments">
-		<h3><?php comments_number('¡Sea el primero en comentar!', 'Un comentario', '% comentarios' );?></h3>
-
-	 <?php else : // comments are closed ?>
-		<!-- If comments are closed. -->
-		<p>Comments are closed.</p>
-
-	<?php endif; ?>
-<?php endif; ?>
-
-<?php if ('open' == $post->comment_status) : ?>
-
-<h3 id="respond">Deje su opini&oacute;n</h3>
-
-<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
-<p>Debe <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">iniciar sesi&oacute;n</a> para comentar.</p>
-<?php else : ?>
-
-<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-
-<?php if ( $user_ID ) : ?>
-
-<p>Comentando como <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>.<br />
-<a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Cerrar sesi&oacute;n &raquo;</a></p>
-
-<?php else : ?>
-
-<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="author">Nombre <?php if ($req) echo "<span>*</span>"; ?></label>
-<br />
-<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="email">Email (no se publicar&aacute;) <?php if ($req) echo "<span>*</span>"; ?></label>
-<br />
-<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-<label for="url">Sitio web</label>
-<br />
-
-<?php endif; ?>
-
-<textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea>
-<br />
-
-<input name="submit" type="submit" id="submit" tabindex="5" value="Publicar" />
-<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-<?php do_action('comment_form', $post->ID); ?>
-
-</form>
-
-<?php endif; // If registration required and not logged in ?>
-
-<?php endif; // if you delete this the sky will fall on your head ?>
-
-</div>
+</div><!-- .comments-area -->
